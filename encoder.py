f273aa0eb43e0887dfde350cdb2b00d367449487
Makoto-Watanabe0909 <waco0909@gmail.com>
2023-02-17 17:43:10 +0900

    -

--- encoder.py
+++ encoder.py
@@ -67,4 +67,4 @@ def shutdown():
     return 'Server shutting down...'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=18920, debug=True, threaded=True)
+    app.run(host="0.0.0.0", port=18920, debug=False, threaded=True)
